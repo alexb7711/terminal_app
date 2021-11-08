cfdf988a15c30f440c6e32b736de98dc1e711968
Alex Brown <42049885+alexb7711@users.noreply.github.com>
2021-11-08 12:14:11 -0700

    Create terminal_app.cpp

---
+++ terminal_app.cpp
@@ -0,0 +1,41 @@
+//
+#include "terminal_app.hpp"
+
+// PUBLIC 
+terminal_app()
+{
+  console();
+  return; 
+}
+
+~terminal_app()
+{
+  return;
+}
+  
+void run()
+{
+  return;
+}
+
+void subscribe()
+{
+  return; 
+}
+  
+// PRIVATE
+
+void console()
+{
+  return; 
+}
+
+void search_cmd()
+{
+  return; 
+}
+
+void execute_cmd()
+{
+  return; 
+}
